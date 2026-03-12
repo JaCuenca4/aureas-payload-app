17805555630070b25fedadb617cdf4074cd38664
jacuencaAS <jordan_cuenca@hotmail.es>
2026-03-12 02:19:38 -0500

    feat: add claudfared

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,6 +4,7 @@ import { buildConfig } from 'payload'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import sharp from 'sharp'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -40,6 +41,22 @@ export default buildConfig({
     url: process.env.DATABASE_URI || '',
   }),
   sharp,
+  plugins: [
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: process.env.R2_BUCKET || '',
+      config: {
+        endpoint: process.env.R2_ENDPOINT || '',
+        credentials: {
+          accessKeyId: process.env.R2_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || '',
+        },
+        region: 'auto',
+      },
+    }),
+  ],
   cors: [process.env.FRONTEND_URL || 'http://localhost:3000'],
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3001',
 })
